9b0f90aefc0b18a916ab43902689846f0c138d83
SatoruGojoMaisForte <satorumec@gmail.com>
2025-02-24 18:57:58 -0300

    Delete libs directory

--- libs/screens/principal_screen/principal_screen.py
+++
@@ -1,6 +0,0 @@
-from kivymd.uix.screen import MDScreen
-
-
-class PerfilScreen(MDScreen):
-    username = 'Baba voss'
-    avatar = 'https://res.cloudinary.com/dsmgwupky/image/upload/v1731366938/image_i6fozn.png'
